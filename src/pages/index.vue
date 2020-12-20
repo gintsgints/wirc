b903286ff6b1be4596fccb35a9f4e19393d1fb40
Gints Polis <gints.polis@balcia.com>
2020-12-20 09:55:41 +0200

    Send button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,33 +3,51 @@
     <div
       class="flex flex-col flex-none justify-between items-center w-16 p-4 bg-gray-200"
     >
-      <div class="flex flex-col space-y-2">
-        <a href="">
-          <div class="rounded-full bg-gray-400 w-8 h-8"></div>
+      <div class="flex flex-col space-y-5">
+        <a class="relative">
+          <svg
+            class="w-9 h-9"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+            ></path>
+          </svg>
+          <div
+            class="absolute top-0 right-0 bg-red-500 w-4 h-4 text-xs text-white rounded-full text-center"
+          >
+            5
+          </div>
         </a>
         <a href="">
-          <div class="rounded-full bg-gray-400 w-8 h-8"></div>
+          <div class="rounded-full bg-gray-400 w-9 h-9"></div>
         </a>
         <a href="">
-          <div class="rounded-full bg-gray-400 w-8 h-8"></div>
+          <div class="rounded-full bg-gray-400 w-9 h-9"></div>
         </a>
       </div>
-      <div class="flex flex-col space-y-2">
+      <div class="flex flex-col space-y-5">
         <a href="">
-          <div class="rounded-full bg-gray-400 w-8 h-8"></div>
+          <div class="rounded-full bg-gray-400 w-9 h-9"></div>
         </a>
         <a href="">
-          <div class="rounded-full bg-gray-400 w-8 h-8"></div>
+          <div class="rounded-full bg-gray-400 w-9 h-9"></div>
         </a>
         <a href="">
-          <div class="rounded-full bg-gray-400 w-8 h-8"></div>
+          <div class="rounded-full bg-gray-400 w-9 h-9"></div>
         </a>
       </div>
     </div>
     <div class="flex flex-auto bg-white rounded-tl-xl shadow-xl">
       <div class="flex flex-none flex-col w-1/5 p-2">
         <div class="flex flex-none justify-between items-center pb-8 mb-4">
-          <h1 class="font-semibold pl-2 text-2xl">Chat</h1>
+          <h1 class="font-semibold pl-2 text-2xl">Chat places</h1>
           <svg
             class="w-6 h-6"
             fill="none"
@@ -161,10 +179,26 @@
               </div>
             </div>
           </div>
-          <div class="flex-none h-40 p-4">
+          <div class="flex flex-row items-center flex-none h-40 p-5 space-x-5">
             <textarea
-              class="outline-none h-full w-full border border-blue-600 border-opacity-0 hover:border-opacity-80 rounded p-4 shadow-lg"
+              class="outline-none h-full flex-auto border border-blue-600 border-opacity-0 hover:border-opacity-80 rounded p-4 shadow-lg"
             ></textarea>
+            <a class="block flex-none w-10">
+              <svg
+                class="w-8 h-8"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
