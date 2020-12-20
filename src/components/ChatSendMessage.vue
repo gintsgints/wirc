028b87637205a2381170281f75e80a4c47266cab
Gints Polis <gints.polis@balcia.com>
2020-12-20 19:15:34 +0200

    Chat messages

---
+++ src/components/ChatSendMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-row items-center flex-none h-40 p-5">
+    <div class="flex-grow">
+      <textarea
+        class="w-full h-full rounded p-4 shadow-lg outline-none border border-blue-600 border-opacity-0 hover:border-opacity-80"
+      ></textarea>
+    </div>
+    <a class="h-full p-5 grid justify-items-center items-center">
+      <Icon icon="carbon:send" style="font-size: 32px" />
+    </a>
+  </div>
+</template>
