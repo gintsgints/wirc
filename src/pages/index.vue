882f3995754be9ebe97ae154589bf5dee7139be3
Gints Polis <gints.polis@balcia.com>
2020-12-20 20:35:06 +0200

    Mobile layout a like

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col md:flex-row h-screen bg-gray-200">
     <Toolbar class="flex-none order-last md:order-first md:w-16 p-4" />
-    <MainChatWindow class="flex-auto order-first md:order-last" />
+    <MainChatWindow
+      class="h-5/6 md:h-full md:flex-auto order-first md:order-last"
+    />
   </div>
 </template>
 
