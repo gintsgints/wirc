09b7ba854b48920372eb2b57d6df562f91834412
Gints Polis <gints.polis@balcia.com>
2020-12-20 12:42:55 +0200

    Fix window overflow on mid size.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -63,11 +63,13 @@
           </div>
         </div>
       </div>
-      <div class="flex flex-row items-center flex-none h-40 p-5 space-x-5">
-        <textarea
-          class="outline-none h-full flex-auto border border-blue-600 border-opacity-0 hover:border-opacity-80 rounded p-4 shadow-lg"
-        ></textarea>
-        <a class="block flex-none w-10">
+      <div class="flex flex-row items-center flex-none h-40 p-5">
+        <div class="flex-grow">
+          <textarea
+            class="w-full h-full rounded p-4 shadow-lg outline-none border border-blue-600 border-opacity-0 hover:border-opacity-80"
+          ></textarea>
+        </div>
+        <a class="h-full p-5 grid justify-items-center items-center">
           <Icon icon="carbon:send" style="font-size: 32px" />
         </a>
       </div>
