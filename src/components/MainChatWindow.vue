64688be5af9ad04f3dd35ba1d8c1449a7f6ed1fd
Gints Polis <gints.polis@balcia.com>
2020-12-22 11:18:30 +0200

    React rework - Get rid of main chat window.

--- src/components/MainChatWindow.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="flex bg-white rounded-tl-xl shadow-xl">
-    <ChatSpaceList
-      class="w-1/5 p-2 flex-none hidden md:block md:overflow-auto"
-    />
-    <Chat />
-  </div>
-</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="flex flex-col md:flex-row h-screen bg-gray-200">
     <Toolbar class="flex-none order-last md:order-first md:w-16 p-4" />
-    <MainChatWindow
-      class="h-5/6 md:h-full md:flex-auto order-first md:order-last"
-    />
+    <div class="h-5/6 md:h-full md:flex-auto order-first md:order-last">
+      <div class="flex bg-white rounded-tl-xl shadow-xl">
+        <ChatSpaceList
+          class="w-1/5 p-2 flex-none hidden md:block md:overflow-auto"
+        />
+        <Chat />
+      </div>
+    </div>
   </div>
 </template>
 
